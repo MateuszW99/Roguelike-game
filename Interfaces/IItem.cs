5c4bab9302b61c71441033eba8316444873f02bd
Eklerek <elekric@gmail.com>
2019-09-27 21:05:00 +0200

    Interface for items

---
+++ Interfaces/IItem.cs
@@ -0,0 +1,14 @@
+﻿using Game.Core;
+
+namespace Game.Interfaces
+{
+    public interface IItem
+    {
+        string Name { get; set; }
+        char Symbol { get; set; }
+
+        void Add(Player player);
+        void Use(Player player, int? itemNumber);
+        
+    }
+}
