97c133729be571f48ba34f9038ca7d7dc19bbf1f
Eklerek <elekric@gmail.com>
2019-09-27 21:39:29 +0200

    Logic for dropping goods from monsters

---
+++ Logic/RandomDrop.cs
@@ -0,0 +1,29 @@
+﻿using Game.Core;
+using Game.Core.Items;
+
+namespace Game.Logic
+{
+    static class RandomDrop
+    {
+        static int numberOfDrops = 2;
+        public static void Next(Monster monster)
+        {
+            int rand = Game.Random.Next(1, 2);
+            switch(rand)
+            {
+                
+                case 1:
+                    {
+                        GoldPile.DropGold(monster);
+                        break;
+                    }
+                case 2:
+                    {
+                        Sword sword = new Sword(monster.X, monster.Y);
+                        sword.DropItem(monster);
+                        break;
+                    }
+            }
+        }
+    }
+}
