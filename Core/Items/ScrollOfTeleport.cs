f01a956f8024a87954c72a0055f26f116406b1a3
Eklerek <mwiczuk99@gmail.com>
2019-10-01 20:06:21 +0200

    Created a new, usable item

--- Core/Items/ScrollOfTeleport.cs
+++ Core/Items/ScrollOfTeleport.cs
@@ -10,22 +10,36 @@ namespace Game.Core.Items
 {
     public class ScrollOfTeleport : ItemActive
     {
+        private static int quantity = 0;
+        public static new int Quantity { get => quantity; set => quantity = value; }
+
         public ScrollOfTeleport(int x, int y) : base(x, y)
         {
+            ScrollOfTeleport.Quantity++;
             Symbol = 'T';
             Name = "Scroll of Teleportation";
         }
 
         public override void GiveEffect()
         {
-            bool teleported = true;
+            bool teleported = false;
             while (!teleported)
             {
                 int randomRoom = Game.Random.Next(0, DungeonMap.Rooms.Count - 1);
                 int destinationX = DungeonMap.Rooms[randomRoom].Center.X + 1;
                 int destinationY = DungeonMap.Rooms[randomRoom].Center.Y + 1;
-                //if()
+                if(DungeonMap.CanTeleport(destinationX, destinationY))
+                {
+                    Teleport(randomRoom);
+                    teleported = true;
+                }
             }
+        }
+
+        private void Teleport(int numberRoom)
+        {
+            Game.Player.X = DungeonMap.Rooms[numberRoom].X;
+            Game.Player.Y = DungeonMap.Rooms[numberRoom].Y;
         }
     }
 
