b227a37258260c18788b0c95b9dd27bfbc4bb914
Eklerek <mwiczuk99@gmail.com>
2019-10-06 18:47:37 +0200

    Renamed to Chestplate.cs

---
+++ Core/Items/Chestplate.cs
@@ -0,0 +1,15 @@
+﻿using Game.Logic.MapGeneration;
+using RogueSharp;
+
+namespace Game.Core.Items
+{
+    public class Chestplate : ItemPassive
+    {
+        public Chestplate(int x, int y) : base(x, y)
+        {
+            Symbol = 'A';
+            Name = "Chestplate";
+            Stats = 1;
+        }
+    }
+}
