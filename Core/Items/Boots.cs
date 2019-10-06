21c5ff33480b21b30ec8cf3fe557b689213dfb96
Eklerek <mwiczuk99@gmail.com>
2019-10-06 19:02:47 +0200

    Boots makes the player's actions faster

---
+++ Core/Items/Boots.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Core.Items
+{
+    public class Boots : ItemPassive
+    {
+        public Boots(int x, int y) : base(x, y)
+        {
+            Symbol = 'B';
+            Name = "Boots";
+            Stats = -1; // Boots give speed and lower speed => more movement
+        }
+    }
+}
